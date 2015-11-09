38b63c1e52243abb5b01f5601e825d624fabd326
Reen Lokum <reen@alphacomm.nl>
2015-11-09 22:06:07 +0100

    Provide latest 5 tweets from a single user

--- src/OhTenPHP/Website/SiteBundle/Controller/DefaultController.php
+++ src/OhTenPHP/Website/SiteBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace OhTenPHP\Website\SiteBundle\Controller;
 
+use Endroid\Twitter\Twitter;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -14,6 +15,14 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return array();
+        /** @var Twitter $twitter */
+        $twitter = $this->get('endroid.twitter');
+        $tweets = $twitter->getTimeline([
+            'count' => 5,
+        ]);
+
+        return [
+            'tweets' => $tweets,
+        ];
     }
 }
