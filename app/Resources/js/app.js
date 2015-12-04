2f36c65867290b77ab708c9da286ccb3ee524729
Roberto Gardenier <roberto@caroga.net>
2015-12-04 08:53:19 +0100

    Implemented better styling.

---
+++ app/Resources/js/app.js
@@ -0,0 +1,23 @@
+jQuery(function ($) {
+    // Carousel slider
+    $(function () {
+        $('#main-slider.carousel').carousel({
+            interval: 10000,
+            pause: false
+        });
+    });
+
+    //smooth scroll
+    $('.navbar-nav > li').click(function (event) {
+        event.preventDefault();
+        var target = $(this).find('>a').prop('hash');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 500);
+    });
+
+    //scrollspy
+    $('[data-spy="scroll"]').each(function () {
+        var $spy = $(this).scrollspy('refresh')
+    });
+});
